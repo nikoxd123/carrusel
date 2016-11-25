5fbd990c99e8fb907f178da27d019b2bfb689f19
DESKTOP-BN69V0N\nikox <nikoxd123@gmail.com>
2016-11-25 18:38:48 -0300

    Documentado e imagen de muestra

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-var count = 0;
+var count = 0; //Cuenta en que posicion del array va
 var slide = document.getElementById("slide1");
 var slideContainer = slide.getElementsByClassName("slide")[0];
-var slideElements = slideContainer.getElementsByClassName("slideElement");
-var numElements = slideElements.length;
-var maxWidth = numElements * 100;
-var isPlay = true;
-var slideTools = slide.getElementsByClassName("sliderTools")[0];
-slideContainer.style = "width:" + maxWidth + "%;";
-var back = slide.getElementsByClassName("back")[0];
-var next = slide.getElementsByClassName("next")[0];
+var slideElements = slideContainer.getElementsByClassName("slideElement"); //Elementos del carrusel
+var numElements = slideElements.length;	//Numeros de elementos en el carrusel
+var maxWidth = numElements * 100;//Maximo de ancho, ejemp: Cuando hay 3 elementos se calcula que el width tiene que ser 300% 
+var isPlay = true; //Se esta reproduciendo el carrusel
+var slideTools = slide.getElementsByClassName("sliderTools")[0]; //Elementos de control del carrusel
+slideContainer.style = "width:" + maxWidth + "%;"; //Se setea el with en el container
+
+var back = slide.getElementsByClassName("back")[0];//Boton Volver
+var next = slide.getElementsByClassName("next")[0];//Boton siguiente
+//Esto Ayuda a que el usuario si no le dio tiempo a leer lo pueda hacer
+//Cuando el mouse esta sobre las herramientas, el carrusel se para
 slideTools.addEventListener("mouseover", function () {
 	isPlay = false;
 	showControllers(true);
 	//console.log("mouse over");
 });
+//Cuando se sca el mouse de las herramientas, el carrusel sigue
 slideTools.addEventListener("mouseout", function () {
 	//console.log("mouse out");
 	showControllers(false);
 	isPlay = true;
 	window.setTimeout(function () { timer(); }, 2500);
 });
+
+//Volver a la anterior imagen
+//--Bug en esta parte del codigo--
 back.addEventListener("click", function () {
 	count--;
 	if (count == -1) {
@@ -27,10 +34,11 @@ back.addEventListener("click", function () {
 	}
 	selectelement();
 });
+//Pasar a la siguiente imagen
 next.addEventListener("click", function () {
 	timer();
 });
-
+//Funcion para el cambio de imagen automaticamente
 function timer() {
 	if (isPlay) {
 		count++;
@@ -43,11 +51,12 @@ function timer() {
 		window.setTimeout(function () { timer(); }, 5000);
 	}
 }
-
-
+//Selecciona la imagen
 function selectelement() {
 	slideContainer.style = "margin-left:" + ((100 * count) * -1) + "%;";
 }
+
+//Oculta y muestra los controles cuando se pone o se saca el mouse de encima.
 function showControllers(show){
 	if(show){
 		back.style = "opacity:1;"
